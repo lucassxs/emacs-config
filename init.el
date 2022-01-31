95d529964fd6f54ab98546ab15e3afe4c0e3ec19
Lucas Stefano <lucassxs.sp@gmail.com>
2022-01-30 23:02:30 -0300

    update config

--- init.el
+++ init.el
@@ -1,57 +1,154 @@
-;; Personal Emacs config
-;; Author: Lucas Stfano
-;; Date: Jan 23 2022
+;; ---------- EMACS GUI Config
 
-
-;; Remover boas vindas
+;; Remove boas vindas
 (setq inhibit-startup-message t)
 
-;; Remover Menus
-(tool-bar-mode -1)
+;; Remove menus
 (menu-bar-mode -1)
-
-;; Remover barra de rolagem
+(tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-;; Número de linhas
+;; numero de linhas
 (global-linum-mode t)
 
+;; Highlight linha atual
+(global-hl-line-mode)
+
 ;; Tamanho da fonte
-(set-face-attribute 'default nil :height 130)
+(set-face-attribute 'default nil :height 120)
 
-;; Pacotes
+;;Flex Buffer
+(defalias 'list-buffers 'ibuffer-other-window)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+
+;; Melpa
 (require 'package)
-(setq package-enable-at-startup nil); desabilitar inicio de ativação
-
-;; Adicionar MELPA :)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-;; Inicia pacotes
+	     '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
-;; Adiciona use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Pacote para testar outros pacotes dentro do emacs :p
+;; ----- Auto complete
+
+; snippets from autocomplete
+(use-package yasnippet
+  :ensure t)
+
+(yas-global-mode 1)
+
+; Company: autocomplete library
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-lenght 1)
+  (global-company-mode t))
+
 (use-package try
   :ensure t)
 
-;; Which key
+(use-package spaceline
+  :ensure t)
+
+(use-package spaceline-config
+  :config
+  (spaceline-emacs-theme))
+
 (use-package which-key
   :ensure t
   :config (which-key-mode))
 
+(use-package all-the-icons
+  :ensure t)
 
-;; Autocomplete
-(use-package auto-complete
+
+;;---------
+
+(use-package ace-window
+  :ensure t
+  :bind (("C-x o" . ace-window)))
+
+(use-package rebecca-theme
+  :ensure t
+  :config (load-theme #'rebecca t))
+
+(use-package counsel
+  :ensure t)
+
+(use-package vterm
+  :ensure t)
+
+(use-package vterm-toggle
+  :ensure t)
+
+(use-package markdown-mode
+  :ensure t)
+
+(global-unset-key (kbd "C-<up>"))
+(global-unset-key (kbd "C-<down>"))
+(use-package move-text
+  :ensure t
+  :config
+  (progn
+    (global-set-key (kbd "C-<up>") 'move-text-up)
+    (global-set-key (kbd "C-<down>") 'move-text-down)))
+
+
+(use-package swiper
   :ensure t
   :init
   (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)))
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t)
+    (global-set-key "\C-f" 'swiper)
+    (global-set-key (kbd "<f1> l") 'counsel-find-library)))
+
+
+;; ----- Discord Rich Presence
+(use-package elcord
+  :ensure t)
+(require 'elcord)
+(elcord-mode)
+
+;; --------- Git config
+
+;; Git extension
+(use-package magit
+  :ensure t)
+
+;;Show diff inline
+(use-package diff-hl
+  :ensure t)
+
+(add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
+(add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
+
+;; Project Organization
+(use-package projectile
+  :ensure t
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+
+
+(setq projectile-project-search-path '("~/git/"))
+(setq projectile-switch-project-action 'neotree-projectile-action)
+
+(use-package neotree
+  :ensure t
+  :config
+  (progn
+    (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+    )
+  :bind (("C-\\". 'neotree-toggle)) ;; atom key
+)
 
 
 
@@ -61,7 +158,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key ## try use-package)))
+ '(package-selected-packages '(use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
